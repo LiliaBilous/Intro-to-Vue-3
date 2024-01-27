f2429093cd3a908f45f5f33f4a53a68de60fed52
LiliaBilous <liliyabilous.email@gmail.com>
2024-01-27 23:22:18 +0200

    L5-end-solution

--- main.js
+++ main.js
@@ -8,6 +8,7 @@ const app = Vue.createApp({
             inventory: 8,
             onSale: false,
             details: ['50% cotton', '30% wool', '20% polyester'],
+            sizes: ['S', 'M', 'L', 'XL'],
             variants: [
                 {id: 2234, color: 'green'},
                 {id: 2235, color: 'blue'}
